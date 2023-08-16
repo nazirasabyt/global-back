6536ef9c2a0073d37b16fd85ef1caae22e365ca1
Nazira <nazirasabyt@gmail.com>
2023-08-16 13:15:32 +0400

    apisalt

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("ADMIN_JWT_SECRET"),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env(`crypto.randomBytes(16).toString('base64')`),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env(`crypto.randomBytes(16).toString('base64')`),
     },
   },
 });
