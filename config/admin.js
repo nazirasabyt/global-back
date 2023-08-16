0c151059c1eb401ee5b4bc1cefb9e56131e81bac
Nazira <nazirasabyt@gmail.com>
2023-08-16 13:22:42 +0400

    new salt

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env("ADMIN_JWT_SECRET"),
+    secret: env("ADMIN_JWT_SECRET", "dsjhvcfk8w4723329841csb"),
   },
   apiToken: {
-    salt: env(`crypto.randomBytes(16).toString('base64')`),
+    salt: env("API_TOKEN_SALT", "jksjdbfiueeer834iw6745true"),
   },
   transfer: {
     token: {
-      salt: env(`crypto.randomBytes(16).toString('base64')`),
+      salt: env("TRANSFER_TOKEN_SALT", "754yeruhfjdn4738iuqe"),
     },
   },
 });
